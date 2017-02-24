b6bdab98771f64bd0a7b55c6b257a6bc373b389d
Robert Elsner <digicoder@gmail.com>
2017-02-24 06:46:15 -0800

    update cookbooks

---
+++ cookbooks/win-templates/metadata.rb
@@ -0,0 +1,10 @@
+name 'win-templates'
+maintainer 'Robert Elsner'
+maintainer_email 'digicoder@gmail.com'
+license 'all_rights'
+description 'Resources run during packer builds'
+long_description 'Resources run during packer builds'
+version '0.1.0'
+
+depends 'windows'
+depends 'wsus-client'

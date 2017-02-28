12dacc51a0c21950ec8e3f20b0e747e484386a18
Robert Elsner <digicoder@gmail.com>
2017-02-28 15:58:00 -0500

    changed reboot responsability to packer

--- cookbooks/win-templates/recipes/enable_containers.rb
+++ cookbooks/win-templates/recipes/enable_containers.rb
@@ -13,9 +13,3 @@ powershell_script 'install_vm_tools' do
     Install-Package -Name docker -ProviderName DockerMsftProvider -Force
     EOH
 end
-
-reboot "app_requires_reboot" do
-    action :request_reboot
-    reason 'Need to reboot for container installation'
-    delay_mins 1
-end

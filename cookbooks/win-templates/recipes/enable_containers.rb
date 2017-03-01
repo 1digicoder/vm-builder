64a8284a84894130a51a90912e899e6090cae48c
Robert Elsner <digicoder@gmail.com>
2017-03-01 00:47:07 -0500

    make containers only install feature

--- cookbooks/win-templates/recipes/enable_containers.rb
+++ cookbooks/win-templates/recipes/enable_containers.rb
@@ -4,12 +4,3 @@ windows_feature "Containers" do
   action :install
   all true
 end
-
-log "Installing Docker..."
-
-powershell_script 'install_vm_tools' do
-    code <<-EOH
-    Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-    Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-    EOH
-end

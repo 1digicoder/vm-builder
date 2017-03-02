085936f32c53faddf2ff9d72022ca9d293cfa435
Robert Elsner <digicoder@gmail.com>
2017-03-02 10:25:38 -0500

    update docker scripts

--- scripts/install-docker.ps1
+++ scripts/install-docker.ps1
@@ -1,7 +1,18 @@
-Write-Host "Installing Docker provider via PSGallery..."
-Import-Module -Name DockerMsftProvider -Force
-Import-Packageprovider -Name DockerMsftProvider -Force
+#Write-Host 'Installing Docker...'
+#Import-Module -Name DockerMsftProvider -Repository PSGallery -Force
+#Import-Packageprovider -Name DockerMsftProvider -Force
+#Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+#Write-Host 'Complete...'
 
-Write-Host "Installing Docker via Microsoft Provider..."
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-Write-Host "Complete"
+$ProgressPreference = 'SilentlyContinue'
+
+Set-ExecutionPolicy Bypass -scope Process
+New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
+wget -outfile $env:TEMP\docker-1.13.1.zip "https://dockermsft.blob.core.windows.net/dockercontainer/docker-1-13-1.zip"
+Expand-Archive -Path $env:TEMP\docker-1.13.1.zip -DestinationPath $env:TEMP -Force
+copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
+Remove-Item $env:TEMP\docker-1.13.1.zip
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
+$env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
+. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
+Start-Service docker

e7e62b5100e8fb2ca448e4875210a1a3243bcc03
Robert Elsner <digicoder@gmail.com>
2017-03-10 01:06:10 -0500

    update path

--- scripts/after-reboot.ps1
+++ scripts/after-reboot.ps1
@@ -1,11 +1,4 @@
 $ProgressPreference='SilentlyContinue'
-Get-WUInstall -WindowsUpdate -AcceptAll -UpdateType Software -IgnoreReboot
-
-# The sleeps may seem whacky because they are
-# Might beable to remove after 2016 RTMs
-# For now after much trial and error, this is what works
-Write-Host "waiting 5 minutes"
-Start-Sleep -Seconds 300
 
 $uninstallSuccess = $false
 while(!$uninstallSuccess) {

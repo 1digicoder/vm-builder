fe5c8f7b14128443c1ed519889b92fd739e930c3
Robert Elsner <digicoder@gmail.com>
2017-03-10 13:35:02 -0500

    fix issue with layout command dying

--- scripts/create_vs2017Layout_enterprise.ps1
+++ scripts/create_vs2017Layout_enterprise.ps1
@@ -1,13 +1,12 @@
 & ..\isoimages\mu_visual_studio_enterprise_2017_x86_x64_10049783.exe `
 --layout C:\isoimages\vs2017_offline\enterprise `
 --lang en-US `
---includeRecommended `
 --add Microsoft.VisualStudio.Workload.Azure `
---add Microsoft.VisualStudio.Workload.Data `
---add Microsoft.VisualStudio.Workload.NetCoreTools `
---add Microsoft.VisualStudio.Workload.NetWeb `
---add Microsoft.VisualStudio.Workload.Node `
---add Microsoft.Net.Component.4.6.2.SDK `
---add Microsoft.Net.Component.4.6.2.TargetingPack `
---add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools `
---add Microsoft.VisualStudio.Component.FSharp
+Microsoft.VisualStudio.Workload.Data `
+Microsoft.VisualStudio.Workload.NetCoreTools `
+Microsoft.VisualStudio.Workload.NetWeb `
+Microsoft.VisualStudio.Workload.Node `
+Microsoft.Net.Component.4.6.2.SDK `
+Microsoft.Net.Component.4.6.2.TargetingPack `
+Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools `
+Microsoft.VisualStudio.Component.FSharp

d0ca926cacf87afde46ca906b0f7498bff073ff0
Robert Elsner <digicoder@gmail.com>
2017-05-02 16:45:41 -0400

    update vs layout

--- scripts/install-vs_enterprise.ps1
+++ scripts/install-vs_enterprise.ps1
@@ -7,8 +7,14 @@ Get-ChildItem -Path ..\isoimages\vs2017_offline\enterprise\certificates -Filter 
 --add Microsoft.VisualStudio.Workload.Data `
 --add Microsoft.VisualStudio.Workload.NetCoreTools `
 --add Microsoft.VisualStudio.Workload.NetWeb `
+--add Microsoft.VisualStudio.Workload.ManagedDesktop `
 --add Microsoft.VisualStudio.Workload.Node `
 --add Microsoft.Net.Component.4.6.2.SDK `
 --add Microsoft.Net.Component.4.6.2.TargetingPack `
 --add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools `
+--add Microsoft.VisualStudio.Component.DiagnosticTools `
+--add Microsoft.VisualStudio.Component.LiveUnitTesting `
+--add Microsoft.VisualStudio.Component.DockerTools `
+--add Microsoft.VisualStudio.Component.IntelliTrace.FrontEnd `
+--add Microsoft.VisualStudio.Component.Git `
 --add Microsoft.VisualStudio.Component.FSharp

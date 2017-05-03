63eb169c77bdd34b3e55c26a4d3a1abaf16fcb3f
Robert Elsner <digicoder@gmail.com>
2017-05-02 20:56:15 -0400

    Update VS installation to add extra needed components

--- scripts/create_vs2017Layout_enterprise.ps1
+++ scripts/create_vs2017Layout_enterprise.ps1
@@ -1,13 +1,24 @@
-& ..\isoimages\mu_visual_studio_enterprise_2017_x86_x64_10049783.exe `
---layout C:\isoimages\vs2017_offline\enterprise `
+& ..\iso-images\mu_visual_studio_enterprise_2017_x86_x64_10049783.exe `
+--layout ..\iso-images\vs2017_offline\enterprise `
 --lang en-US `
 --add Microsoft.VisualStudio.Workload.Azure `
 Microsoft.VisualStudio.Workload.Data `
-Microsoft.VisualStudio.Workload.ManagedDesktop `
 Microsoft.VisualStudio.Workload.NetCoreTools `
 Microsoft.VisualStudio.Workload.NetWeb `
+Microsoft.VisualStudio.Workload.ManagedDesktop `
 Microsoft.VisualStudio.Workload.Node `
+Microsoft.Net.Component.4.TargetingPack `
 Microsoft.Net.Component.4.6.2.SDK `
+Microsoft.Net.Component.4.6.TargetingPack `
 Microsoft.Net.Component.4.6.2.TargetingPack `
 Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools `
+Microsoft.Net.Component.4.5.1.TargetingPack `
+Microsoft.VisualStudio.Component.DiagnosticTools `
+Microsoft.VisualStudio.Component.CodeClone `
+Microsoft.VisualStudio.Component.CodeMap `
+Microsoft.VisualStudio.Component.LiveUnitTesting `
+Microsoft.VisualStudio.Component.DockerTools `
+Microsoft.VisualStudio.Component.IntelliTrace.FrontEnd `
+Microsoft.VisualStudio.Component.Git `
+Microsoft.NetCore.ComponentGroup.DevelopmentTools `
 Microsoft.VisualStudio.Component.FSharp
--- scripts/install-vs_enterprise.ps1
+++ scripts/install-vs_enterprise.ps1
@@ -1,6 +1,6 @@
-Get-ChildItem -Path ..\isoimages\vs2017_offline\enterprise\certificates -Filter *.p12 | Import-PfxCertificate -CertStoreLocation cert:\localMachine\my
+Get-ChildItem -Path ..\iso-images\vs2017_offline\enterprise\certificates -Filter *.p12 | Import-PfxCertificate -CertStoreLocation cert:\localMachine\my
 
-& ..\isoimages\vs2017_offline\enterprise\vs_enterprise.exe `
+& ..\iso-images\vs2017_offline\enterprise\vs_enterprise.exe `
 --passive `
 --norestart `
 --add Microsoft.VisualStudio.Workload.Azure `
@@ -9,12 +9,18 @@ Get-ChildItem -Path ..\isoimages\vs2017_offline\enterprise\certificates -Filter 
 --add Microsoft.VisualStudio.Workload.NetWeb `
 --add Microsoft.VisualStudio.Workload.ManagedDesktop `
 --add Microsoft.VisualStudio.Workload.Node `
+--add Microsoft.Net.Component.4.TargetingPack `
 --add Microsoft.Net.Component.4.6.2.SDK `
+--add Microsoft.Net.Component.4.6.TargetingPack `
 --add Microsoft.Net.Component.4.6.2.TargetingPack `
+--add Microsoft.Net.Component.4.5.1.TargetingPack `
 --add Microsoft.Net.ComponentGroup.4.6.2.DeveloperTools `
 --add Microsoft.VisualStudio.Component.DiagnosticTools `
+--add Microsoft.VisualStudio.Component.CodeClone `
+--add Microsoft.VisualStudio.Component.CodeMap `
 --add Microsoft.VisualStudio.Component.LiveUnitTesting `
 --add Microsoft.VisualStudio.Component.DockerTools `
 --add Microsoft.VisualStudio.Component.IntelliTrace.FrontEnd `
 --add Microsoft.VisualStudio.Component.Git `
+--add Microsoft.NetCore.ComponentGroup.DevelopmentTools `
 --add Microsoft.VisualStudio.Component.FSharp
